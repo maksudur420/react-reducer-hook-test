187dc911c1326c7e3376f11f1f4bf1134f13052b
maksudur420 <mdmaksudur588@gmail.com>
2023-09-22 13:06:14 +0600

    usereducer use as object way

--- src/Counter.js
+++ src/Counter.js
@@ -2,14 +2,16 @@ import React, { useReducer } from 'react';
 import './counter.css'
 
 
-const initialState =5;
+const initialState ={
+    counter:5,
+};
 
 const reducer =(state,action)=>{
-    switch (action) {
+    switch (action.type) {
         case 'increment':
-            return state +1;
+            return {counter: state.counter +action.value}
         case 'decrement':
-            return state -1;
+            return {counter: state.counter -action.value}
         default:
            return state;
     }
@@ -20,9 +22,9 @@ const Counter = () => {
     const [count,dispatch]=useReducer(reducer,initialState);
     return (
         <div className='counter'>
-            <div className=''>{count}</div>
-            <button type='button' className='btn' onClick={()=>dispatch('increment')}>INCREMENT</button>
-            <button type='button' className='btn' onClick={()=>dispatch('decrement')}>DECREMENT</button>
+            <div className=''>{count.counter}</div>
+            <button type='button' className='btn' onClick={()=>dispatch({type:"increment",value: 6})}>INCREMENT</button>
+            <button type='button' className='btn' onClick={()=>dispatch({type:"decrement",value:2})}>DECREMENT</button>
         </div>
     );
 };
